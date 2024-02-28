d4e9c832d8904c6eb3fe5d372f397ce176685f9e
engektiar <eng.ektiar@gmail.com>
2024-02-28 23:50:25 +0600

    home page desing done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./page/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4C60BF",
+      },
+    },
+  },
+  plugins: [],
+};
